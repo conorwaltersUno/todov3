8204029d9176c853ff472b054c6f0e6222db9633
Cheesiewalters <cwalters01@qub.ac.uk>
2022-06-10 13:43:30 +0100

    Modal opening when card clicked

---
+++ frontend/src/pages/home/components/modalComponent.js
@@ -0,0 +1,42 @@
+import { Button, Modal, Typography } from "@material-ui/core";
+import { Box } from "@mui/system";
+import React from "react";
+
+const ModalComponent = ({
+  open,
+  handleClose,
+  todo: { id, header, description, todotask },
+}) => {
+  const style = {
+    position: "absolute",
+    top: "20%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 700,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {id}.{header}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {description}
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalComponent;
--- frontend/src/pages/home/components/todoCard.js
+++ frontend/src/pages/home/components/todoCard.js
@@ -1,10 +1,15 @@
-import { Card, Typography, CardContent } from "@material-ui/core";
+import { Card, Typography, CardContent, Modal, Box } from "@material-ui/core";
 import React from "react";
+import ModalComponent from "./modalComponent";
 
 const TodoCard = ({ todo }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <div>
-      <Card sx={{ minWidth: 275 }}>
+    <div className={"card-container"}>
+      <Card onClick={handleOpen}>
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             {todo.header}
@@ -12,6 +17,13 @@ const TodoCard = ({ todo }) => {
           <Typography variant="body2">{todo.description}</Typography>
         </CardContent>
       </Card>
+      <ModalComponent
+        open={open}
+        todo={todo}
+        handleClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      ></ModalComponent>
     </div>
   );
 };
