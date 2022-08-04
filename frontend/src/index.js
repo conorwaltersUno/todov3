987b258342c1f40fc887a49164b02c9e66c58e96
cwalters <cwalters@foundationmedicine.com>
2022-08-04 12:17:47 +0100

    redeploy frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,6 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
-
 ReactDOM.render(
   <React.StrictMode>
     <Router>
